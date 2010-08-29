43e2dcfbb6f0049b0946568fff77c37fecce41b1
Abhinav <abhinav@abhinav-laptop.(none)>
2010-08-29 21:38:33 +0530

    Get parameters getting parsed! No Issues left.

--- mod_hoverin.c
+++ mod_hoverin.c
@@ -7,17 +7,16 @@
 #include <apr_hash.h>
 
 
-/* All function names starting with hoverin : like a namespace */
 module AP_MODULE_DECLARE_DATA hoverin_module  ;
 
-/* per-dir-configuration data structure for the module */
+/** per-dir-configuration data structure for the module */
 typedef struct {
 	const char *header;
 	const char *footer;
 	apr_table_t *hosts;
 } hoverin_dir_cfg;
 
-/* filter context data structure: f->ctx */
+/** filter context data structure: f->ctx */
 typedef struct {
 	apr_bucket *head;
 	apr_bucket *foot;
@@ -25,23 +24,23 @@ typedef struct {
 	unsigned int state;
 } hoverin_ctx;
 
+/** A request config data structure to store the get parameters from the
+ *  request.
+ */
 typedef struct {
-	apr_table_t *params_table
+	apr_table_t *params_table;
 } r_cfg;
-/** A hash table for storing get parameters of the request */
-//static apr_table_t *params_table;
 
 
 /* Name of the filter-module */
 static const char *name = "hoverin-filter";
 
-//static int count = 0;
-
 
 #define TXT_HEAD 0x01
 #define TXT_FOOT 0x02
 
-/*  a utility function that reads the header and footer files and returns them 
+
+/**  a utility function that reads the header and footer files and returns them
 *	in the form of a file bucket
 *	params:
 *		@r: a request_rec object that represents the current request_rec
@@ -52,10 +51,12 @@ static apr_bucket* hoverin_file_bucket(request_rec *r, const char *fname)
 {
     apr_file_t *file = NULL;
 	apr_finfo_t finfo;
+	/* stat the size of the file without opening it */
 	if ( apr_stat(&finfo, fname, APR_FINFO_SIZE, r->pool) != APR_SUCCESS ) {
 		ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r, "mod_hoverin: stat error");
 		return NULL;
 	}
+	/* open the file in sharedlock mode */
 	if ( apr_file_open(&file, fname, APR_READ | APR_SHARELOCK | 
 		APR_SENDFILE_ENABLED, APR_OS_DEFAULT, r->pool) != APR_SUCCESS ) {
 		ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r, 
@@ -74,10 +75,10 @@ static apr_bucket* hoverin_file_bucket(request_rec *r, const char *fname)
 
 
 /**
-	A simplified implementation of a get parameter parser.
-	It simply takes the querystring from the request object and stores the
-	parameters in a hash table in key value format. No support for multiple
-	valued parameters as is the case in our module.
+*	A simplified implementation of a get parameter parser.
+*	It simply takes the querystring from the request object and stores the
+*	parameters in a hash table in key value format. No support for multiple
+*	valued parameters as is the case in our module.
 */
 static void parse_get_params(request_rec *r, const char *querystring)
 {
@@ -87,20 +88,22 @@ static void parse_get_params(request_rec *r, const char *querystring)
 	char *ch;
 	char *value;
 	const char *token;
-	//params_table = apr_table_make(r->pool, 6);
-	//apr_table_t *params_table = ap_get_module_config(r->request_config, &hoverin_module);
+
 	r_cfg *my_r_cfg = ap_get_module_config(r->request_config, &hoverin_module);
-	my_r_cfg->params_table = apr_table_make(r->pool, 6);
 	apr_table_t *params_table = my_r_cfg->params_table;
-	
+	if (my_r_cfg->params_table == NULL) {
+			ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r, "params_table is NULL!");
+			return;
+	}
+	/* if there is no querystring attached with this url, then leave */
 	if (querystring == NULL) {
 		return;
 	}
 	
-	/*for (pair = (char *) apr_strtok(querystring, delim, &last);
+	for (pair = (char *) apr_strtok(querystring, delim, &last);
 		 pair != NULL; 
-		 pair = (char *) apr_strtok(NULL, delim, &last)) {*/
-	while (*querystring && (pair = ap_getword(r->pool, (const char **) &querystring, '&'))) {
+		 pair = (char *) apr_strtok(NULL, delim, &last)) {
+
 		/**
 		*	replace + in the current key-value pair with blank spaces 
 		*/
@@ -111,7 +114,7 @@ static void parse_get_params(request_rec *r, const char *querystring)
 			}
 		}
 		
-		/**
+		/*
 		*	Now separate the key and value and store them in the table 
 		*/
 		ch = NULL;
@@ -130,19 +133,18 @@ static void parse_get_params(request_rec *r, const char *querystring)
 		ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r, "key=%s, val=%s", pair, ch);
 		apr_table_set(params_table, pair, ch);
 	}
-	//return params_table;
+
 }
 
+/**
+ * Function to initialize the request config structure.
+ */
 static void parse_query(request_rec *r)
 {
-	
 	r_cfg *my_r_cfg = apr_palloc(r->pool, sizeof(r_cfg));
-	//apr_table_t	*params_table = apr_table_make(r->pool, 6);
-	//ap_set_module_config(r->request_config, &hoverin_module, params_table);
 	ap_set_module_config(r->request_config, &hoverin_module, my_r_cfg);
-	my_r_cfg->params_table = NULL;
-	//parse_get_params(r, r->parsed_uri.query);
-	
+	my_r_cfg->params_table = apr_table_make(r->pool, 6);
+
 }
 
 /**
@@ -163,21 +165,18 @@ static const char *get_parameter(request_rec *r, const char *param)
 		ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r, "r or param is null");
 		return;
 	}
-	//apr_table_t *params_table = NULL;
-	//apr_table_t *params_table = ap_get_module_config(r->request_config, &hoverin_module);
-	//apr_table_t *params_table = apr_table_make(r->pool, 6);
-	//parse_get_params(r, r->parsed_uri.query);
+
 	r_cfg  *my_r_cfg = ap_get_module_config(r->request_config, &hoverin_module);
 	apr_table_t *params_table = my_r_cfg->params_table;
 	if (params_table == NULL) {
-		ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r, "table is null");
-		//params_table = parse_get_params(r, r->parsed_uri.query);
+		ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r, "params_table is null");
+		return NULL;
 	}
 	
 	
 	const char *val = apr_table_get(params_table, param);
 	/**
-	*	If the value of the parameter is NULL then return ''
+	*	If the value of the parameter is NULL then return empty string.
 	*/
 	if (val == NULL) {
 		return "";
@@ -201,65 +200,66 @@ static void insert_text(request_rec *r, apr_bucket_brigade *bb, const char *plac
 	apr_bucket *b;
 	const char *buf;
 	const char *tag;
-	int sz, flag = 0;
+	int sz;
+	int flag = 0;
 	size_t offset = 0;
-	
-	
-														
+
 	/* loop through the bucket brigade and read each bucekt to scan for the
 	*	string place
 	*/
 	for (b = APR_BRIGADE_FIRST(bb) ;
 		 b != APR_BRIGADE_SENTINEL(bb) ;
 		 b = APR_BUCKET_NEXT(b) ) {
+		/* If metabuckets, do not read them */
 		 if (APR_BUCKET_IS_EOS(b) || APR_BUCKET_IS_FLUSH(b))
 		 	continue;
 		 else {
-		apr_bucket_read(b, &buf, &sz, APR_BLOCK_READ);
-		if (buf == NULL) {
-			ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r, "mod_hoverin: buf = NULL");
-			return;
-		}
-		
-		/* check if the buffer contains place,
-		*  ap_strcasestr will return the pointer to the starting of place
-		*  if it finds it, else NULL.
-		*/
-		tag = ap_strcasestr(buf, place);
-		if (tag != NULL) {
-						
-			/*	calculate the position (offset) where to split the bucket-
-			*	We want to split the bucket at the point where place starts in
-			*	the bucket, so we find the difference between the starting
-			*	address of the buffer (buf) and starting address of place 
-			*	(tag),that will be the offset of place within the bucket
-			*/
-			offset = ((unsigned int) tag - (unsigned int) buf )/sizeof(char) ;
+			apr_bucket_read(b, &buf, &sz, APR_BLOCK_READ);
+			if (buf == NULL) {
+				ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
+						"mod_hoverin: buf = NULL");
+				return;
+			}
 			
-			/*  split the bucket at the calculated offset.
-			*	The current bucket will be broken down into 2 buckets,
-			*	the first bucket will contain data upto the point of offset,
-			*	rest of data will be in the next (newly created) bucket.
+			/* check if the buffer contains place,
+			*  ap_strcasestr will return the pointer to the starting of place
+			*  if it finds it, else NULL.
 			*/
-			apr_bucket_split(b, offset);
-			
-			/*	after the split the first bucket will contain data before
-			* 	place and the next bucket will containg data starting from
-			*	place and onwards. So we will move to the NEXT bucket,
-			*	and insert a new bucket (that contains the text) before it.
-			*/ 
-			b = APR_BUCKET_NEXT(b);
-			APR_BUCKET_INSERT_BEFORE(b,
-				  					 apr_bucket_heap_create(
-				  					 (const char *)text, strlen(text),
-				  					 NULL,
-				  					 r->connection->bucket_alloc ));
-			
-			/* 	Increment the flag, so that we may know that place was found
-			*	and we can break out from the loop 
-			*/
-			flag++;
-		}
+			tag = ap_strcasestr(buf, place);
+			if (tag != NULL) {
+
+				/*	calculate the position (offset) where to split the bucket-
+				*	We want to split the bucket at the point where place starts
+				*	in the bucket, so we find the difference between the starting
+				*	address of the buffer (buf) and starting address of place
+				*	(tag),that will be the offset of place within the bucket
+				*/
+				offset = ((unsigned int) tag - (unsigned int) buf )/sizeof(char) ;
+
+				/*  split the bucket at the calculated offset.
+				*	The current bucket will be broken down into 2 buckets,
+				*	the first bucket will contain data upto the point of offset,
+				*	rest of data will be in the next (newly created) bucket.
+				*/
+				apr_bucket_split(b, offset);
+
+				/*	after the split the first bucket will contain data before
+				* 	place and the next bucket will containg data starting from
+				*	place and onwards. So we will move to the NEXT bucket,
+				*	and insert a new bucket (that contains the text) before it.
+				*/
+				b = APR_BUCKET_NEXT(b);
+				APR_BUCKET_INSERT_BEFORE(b,
+										 apr_bucket_heap_create(
+										 (const char *)text, strlen(text),
+										 NULL,
+										 r->connection->bucket_alloc ));
+
+				/* 	Increment the flag, so that we may know that place was found
+				*	and we can break out from the loop
+				*/
+				flag++;
+			}
 		}
 		if (flag) {
 			return;
@@ -278,11 +278,11 @@ static void add_comment(request_rec *r, apr_bucket_brigade *bb)
 													 (const char *) r->uri, r);
 	
 	/* build the comment by concatenating the url and the comment syntax */
-	const char *my_comment = (const char *) apr_pstrcat(r->connection->pool, 
+	const char *my_comment = (const char *) apr_pstrcat(r->pool,
 							"<!-- ", url, " -->", NULL);
 	
 	/** the place where comment is to be inserted */
-	const char *place = (const char *) apr_psprintf(r->connection->pool,
+	const char *place = (const char *) apr_psprintf(r->pool,
 													 "</head>");
 																
 	/** insert the comment before the </head> */
@@ -298,37 +298,54 @@ static void modify_header(request_rec *r, apr_bucket_brigade *bb)
 	apr_uri_t *uri = &(r->parsed_uri);
 	const char *host = (const char *) r->hostname;
 	char *event = ap_getword(r->pool, (const char **) &host, '.');
-	
-	const char *path = uri->path;
-	path++;
-	
-	const char *url, *referrer, *site, *theme, *hid, *category;
-	const char *part, *type, *nick, *hoverlet, *param1;
-	
+	const char *url;
+	const char *referrer;
+	const char *site;
+	const char *theme;
+	const char *hid;
+	const char *category;
+	const char *part;
+	const char *type;
+	const char *nick;
+	const char *hoverlet;
+	const char *param1;
 	const char *params = (const char *) apr_psprintf(r->pool, " ");
 	int i = 0, j = 0, part_count;
 	apr_hash_t *parsed_path = apr_hash_make(r->pool);
-	while (*path && (part = ap_getword(r->pool, &path, '/'))) {
-		apr_hash_set(parsed_path, &i, sizeof(i), part);
-		i++;
+	const char *path = uri->path;
+	/* parse the path only if it is not empty */
+	if (path++ != "/") {
+		while (*path && (part = ap_getword(r->pool, &path, '/'))) {
+			apr_hash_set(parsed_path, &i, sizeof(i), part);
+			i++;
+		}
 	}
-	/**
-		save the number of entries in the hash table for future use
+
+	/*
+	*	save the number of entries in the hash table for future use
 	*/
 	part_count = i;
 	i = 0;
-	/**
-		get the various field values from the hash table into some local 
-		variables, only for code conciseness as we need to concatenate these
-		values with a very large string.
+	/*
+	*	get the various field values from the hash table into some local
+	*	variables, only for code conciseness as we need to concatenate these
+	*	values with a very large string.
 	*/
-	type = apr_hash_get(parsed_path, &i, sizeof(i));
+	type = apr_hash_get(parsed_path, &i, sizeof(i)) != NULL ?
+			apr_hash_get(parsed_path, &i, sizeof(i)):
+			"";
 	i++;
-	nick = apr_hash_get(parsed_path, &i, sizeof(i));
+	nick = apr_hash_get(parsed_path, &i, sizeof(i)) != NULL ?
+			apr_hash_get(parsed_path, &i, sizeof(i)):
+			"";
 	i++;
-	hoverlet = apr_hash_get(parsed_path, &i, sizeof(i));
+	hoverlet = apr_hash_get(parsed_path, &i, sizeof(i)) != NULL ?
+				apr_hash_get(parsed_path, &i, sizeof(i)):
+				"";
 	i++;
-	param1 = apr_hash_get(parsed_path, &i, sizeof(i));
+	param1 = apr_hash_get(parsed_path, &i, sizeof(i)) != NULL ?
+				apr_hash_get(parsed_path, &i, sizeof(i)):
+				"";
 	i++;
 	
 	/**
@@ -347,7 +364,7 @@ static void modify_header(request_rec *r, apr_bucket_brigade *bb)
 		i++;
 	}
 	
-	/**
+	/*
 	*	Extract the rest of the field values from request parameters
 	*/
 
@@ -359,10 +376,8 @@ static void modify_header(request_rec *r, apr_bucket_brigade *bb)
 	category = get_parameter(r, "cat");
 
 
-	ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r, "theme = %s", theme);
-	
-	/**
-		Now put all the filed values inside the HOVER variable.
+	/*
+	*	Now put all the filed values inside the HOVER variable.
 	*/
 	const char *hover = (const char *)apr_pstrcat(r->pool,
 		"var HOVER = { event:\'", event, "\' kw: window.decodeURIComponent(\'", 
@@ -380,7 +395,7 @@ static void modify_header(request_rec *r, apr_bucket_brigade *bb)
 }
 
 
-/* per directory configuration initialisation function called by Apache
+/** per directory configuration initialisation function called by Apache
 *	to initialise configuration vectors of the module
 */
 static void* hoverin_dir_config(apr_pool_t *pool, char *val)
@@ -402,7 +417,7 @@ static void* hoverin_dir_merge(apr_pool_t *pool, void *old, void *new)
 }
 
 
-/* filter initialization function */
+/** filter initialization function */
 static int hoverin_filter_init(ap_filter_t *f)
 {
 	hoverin_ctx *ctx = f->ctx = apr_palloc(f->r->pool, sizeof(hoverin_ctx));
@@ -415,29 +430,36 @@ static int hoverin_filter_init(ap_filter_t *f)
 	return OK;
 }
 
-/* the main filter callback funtion */
+/** the main filter callback funtion */
 static int hoverin_filter(ap_filter_t *f, apr_bucket_brigade *bb)
 {
 	apr_bucket *b;
 	const char *current_host = f->r->hostname;
-	int count = 0;
 	apr_off_t length;
-	//ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, f->r, "count = %d", count++);
-	apr_table_unset(f->r->headers_out, "Content-Length");
 	hoverin_ctx *ctx = (hoverin_ctx *) f->ctx;
+	/* Initialise the filter context (ctx) if it hasn't been initialised yet */
 	if (ctx == NULL) {
 		hoverin_filter_init(f);
 		ctx = (hoverin_ctx *) f->ctx;
 	}
 	
-	/** Check if the module should work for this host or not */												 
+	/* Check if the module should work for this host or not */
 	if (apr_table_get(ctx->hosts, current_host) == NULL) {
 		ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, f->r, "mod_hoverin: "
 						"Invalid host: %s", current_host);
 		return ap_pass_brigade(f->next, bb);
 		
 	}
+
+	/* Now when we are going to process this request, we should unset the
+	 * Content-Length response header, and set it later after the processing
+	 * is complete.
+	 */
+	apr_table_unset(f->r->headers_out, "Content-Length");
+
+	/* setup the request_config object */
 	parse_query(f->r);
+
 	/*	Insert the footer at the end of the brigade. */
 	if ( ctx->foot && ! ( ctx->state & TXT_FOOT ) ) {
 			b = APR_BRIGADE_LAST(bb) ;
@@ -456,28 +478,23 @@ static int hoverin_filter(ap_filter_t *f, apr_bucket_brigade *bb)
 	}
 	
 	/* Header and Footer added to the response, now add the comment */
-	//if (count == 0) {
-
-	/*apr_table_t *params_table = ap_get_module_config(f->r->request_config,
-										&hoverin_module);*/
 	r_cfg *my_r_cfg = ap_get_module_config(f->r->request_config, &hoverin_module);
 	apr_table_t *params_table = my_r_cfg->params_table;
-	if (params_table == NULL) {
-		ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, f->r, "parsing query");
-		//parse_query(f->r);
-		parse_get_params(f->r, f->r->parsed_uri.query);
-		add_comment(f->r, bb);
-		modify_header(f->r, bb);
-	//}
-	}
-	count++;
+	ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, f->r, "parsing query");
+	add_comment(f->r, bb);
+	parse_get_params(f->r, f->r->parsed_uri.query);
+	modify_header(f->r, bb);
+
+
+	/* The length of the new response can be found from apr_brigade_length() */
 	apr_brigade_length(bb, 1, &length);
-	apr_table_set(f->r->headers_out, "Content-Length", (const char *) apr_itoa(f->r->pool, length));
+	apr_table_set(f->r->headers_out, "Content-Length",
+			(const char *) apr_itoa(f->r->pool, length));
 
 	return ap_pass_brigade(f->next, bb);
-	//return OK;
-}
 
+}
+/** implementing the mod_hoverin_hosts directive */
 static const char *set_allowed_hosts(cmd_parms *parms, void *dummy, 
 									const char *arg)
 {
@@ -487,7 +504,7 @@ static const char *set_allowed_hosts(cmd_parms *parms, void *dummy,
 }
 
 
-/* following is the regular Apache Module stuff, the rudimentary functions 
+/** following is the regular Apache Module stuff, the rudimentary functions
 *	required (in every filter module) to configure the module,
 *	setting callbacks, etc 
 */
@@ -504,18 +521,14 @@ static const command_rec hoverin_cmds[] = {
 	{ NULL }
 };
 
-/* setting the hooks */
+/** setting the hooks */
 static void hoverin_hooks(apr_pool_t *pool)
 {
-	//ap_hook_post_read_request(parse_query, NULL, NULL, APR_HOOK_FIRST);
-	//ap_register_output_filter(name, hoverin_filter, hoverin_filter_init, AP_FTYPE_RESOURCE);
-	ap_register_output_filter_protocol(name, hoverin_filter,
-			hoverin_filter_init, AP_FTYPE_RESOURCE,
-			AP_FILTER_PROTO_CHANGE|AP_FILTER_PROTO_CHANGE_LENGTH) ;
-	  //ap_hook_post_config(line_edit, NULL, NULL, APR_HOOK_MIDDLE) ;
+	ap_register_output_filter(name, hoverin_filter, hoverin_filter_init,
+			AP_FTYPE_RESOURCE);
 }
 
-/* the module data structure */
+/** the module data structure */
 module AP_MODULE_DECLARE_DATA hoverin_module = {
 
 	STANDARD20_MODULE_STUFF,
